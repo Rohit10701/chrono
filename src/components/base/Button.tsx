7292f8116549661db0be31ac67a13520db44ab63
Rohit10701 <rohitkumar10701@gmail.com>
2024-03-09 21:12:26 +0530

    chore : restructuing the variant to indiviual file

--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -1,9 +1,27 @@
 "use client";
 import { cn } from "@/libs/utils";
-import buttonVariant from "@/variants/base/Button";
-import { VariantProps } from "class-variance-authority";
+import { VariantProps, cva } from "class-variance-authority";
 import React, { ButtonHTMLAttributes } from "react";
-
+const buttonVariant = cva(
+  "h-[40px] pl-3 inline-flex items-center justify-center rounded-md text-sm font-medium focus:outline-none ",
+  {
+    variants: {
+      variant: {
+        dark: "bg-black text-white border-2 border-white border-input hover:bg-accent hover:text-accent-foreground",
+        white:
+          "bg-white text-black  border-2 border-black bg-secondary text-secondary-foreground hover:bg-secondary/80",
+      },
+      size: {
+        sm: "h-11 px-4 rounded-md w-56",
+        lg: "h-11 px-4 rounded-md w-72",
+      },
+    },
+    defaultVariants: {
+      variant: "dark",
+      size: "lg",
+    },
+  }
+);
 interface ButtonProps
   extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "size">,
     VariantProps<typeof buttonVariant> {
--- src/components/base/Input.tsx
+++ src/components/base/Input.tsx
@@ -1,20 +1,50 @@
 "use client";
-import React, { ChangeEvent, HTMLProps, forwardRef, useEffect, useState } from "react";
-import { VariantProps } from "class-variance-authority";
+import React, {
+  ChangeEvent,
+  HTMLProps,
+  forwardRef,
+  useEffect,
+  useState,
+} from "react";
+import { VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/libs/utils";
 import { useDebounce } from "@/hooks/use-debounce";
-import inputBoxVariant  from "@/variants/base/Input";
 
-
+const inputBoxVariant = cva(
+  "h-[40px] pl-3 inline-flex items-center justify-center rounded-md text-sm font-medium focus:outline-none ",
+  {
+    variants: {
+      variant: {
+        dark: "bg-black text-white border-2 border-white border-input hover:bg-accent hover:text-accent-foreground",
+        white:
+          "bg-white text-black  border-2 border-black bg-secondary text-secondary-foreground hover:bg-secondary/80",
+      },
+      size: {
+        sm: "h-11 px-4 rounded-md w-56",
+        lg: "h-11 px-4 rounded-md w-72",
+      },
+    },
+    defaultVariants: {
+      variant: "dark",
+      size: "lg",
+    },
+  }
+);
 interface Props<T>
   extends Omit<HTMLProps<HTMLInputElement>, "size">,
     VariantProps<typeof inputBoxVariant> {
   inputText: T;
   setInputText: (value: T) => void;
-  
 }
 
-const Input = ({ inputText, setInputText, className, variant, size, ...inputProps }: Props<string>) => {
+const Input = ({
+  inputText,
+  setInputText,
+  className,
+  variant,
+  size,
+  ...inputProps
+}: Props<string>) => {
   const [debouncedInput, setDebouncedInput] = useState("");
 
   const debouncedText = useDebounce<string>({ value: debouncedInput });
